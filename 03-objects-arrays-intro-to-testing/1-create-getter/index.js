17d19653f870e75f3ad8bb9db78d518468db1418
Gregory Kirillov <kgn.0@ya.ru>
2022-09-06 21:17:19 +0300

    1-create-getter solved

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,12 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  return (obj) => {
+    const props = path.split('.');
+    let tempObj = obj;
+  
+    props.forEach(prop => tempObj = tempObj?.[prop]);
+  
+    return tempObj;
+  };
 }
