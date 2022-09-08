43c8513fef235e4a86bc2fdd519d95699ca9a10e
Gregory Kirillov <kgn.0@ya.ru>
2022-09-08 14:39:02 +0300

    removed recalling method

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,8 +4,8 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const props = path.split('.');
   return (obj) => {
-    const props = path.split('.');
     let tempObj = obj;
   
     props.forEach(prop => tempObj = tempObj?.[prop]);
